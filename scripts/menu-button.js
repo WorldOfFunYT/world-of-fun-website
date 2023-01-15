1632b14d5d089e67e6d329c4dcc86e5fccb9b338
WorldOfFunYT <czarczynskiszymon@gmail.com>
2023-01-15 19:09:12 +0000

    Made the menu less buggy

--- scripts/menu-button.js
+++ scripts/menu-button.js
@@ -4,23 +4,6 @@ let menuOpen = false;
 menuBtn.addEventListener(
     "click",
     () => {
-        // if (!menuOpen) {
-        //     menu.classList.remove("closed");
-        //     menuBtn.classList.add("open");
-        //     menu.classList.add("open");
-        //     menuOpen = true;
-        // } else {
-        //     menuBtn.classList.remove("open");
-        //     menu.classList.remove("open");
-        //     menuOpen = false;
-        //     menu.addEventListener(
-        //         "transitionend",
-        //         () => {
-        //             console.log("here");
-        //             menu.classList.add("closed");
-        //         }, { once: true }
-        //     );
-        // }
         if (menu.classList.contains("hidden")) {
             menuBtn.classList.add("open");
             menu.classList.remove("hidden");
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,35 @@
 const menu = document.querySelector("[data-menu]");
+const menuItems = document.querySelector("[data-menu-items]");
+
+console.log(`Inner html:           ${menuItems.innerHTML}`);
+const menuLinks = [{
+        name: "home",
+        link: "./",
+    },
+    {
+        name: "about",
+        link: "",
+    },
+    {
+        name: "music",
+        link: "./music.html",
+    },
+];
+
+menuItems.innerHTML = "";
+
+for (const link of menuLinks) {
+    if (link.link == "") {
+        link.link = "#";
+    }
+    menuItems.innerHTML += `<a href="${link.link}" class="menu-item" data-menu-item>${link.name}</a>`;
+}
 
 Array.from(document.querySelectorAll("[data-menu-item]")).forEach(
     (item, index) => {
         item.onmouseover = () => {
             menu.dataset.activeIndex = index;
+            menu.style.setProperty("--bg-pos-y", index * -25 - 25 + "%");
         };
     }
 );
